50deb103c4ed2d91228d9873cebdcc2186ff78fd
null_ptr <rhannek@gmx.de>
2014-02-10 02:08:48 +0100

    Added default pw = none

--- libkeksbot/server.h
+++ libkeksbot/server.h
@@ -35,7 +35,7 @@ public:
 	void EventNumeric(unsigned int       evt, const std::string& origin, const ParamList& args);
 
 	bool IsConnected(void);
-	void Join(const std::string& chan, const std::string& pw);
+	void Join(const std::string& chan, const std::string& pw = "");
 };
 
 #endif
