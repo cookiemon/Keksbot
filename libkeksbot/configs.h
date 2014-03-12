9b576752b357fa7985b1e21d73e3cc5fd43bd6d1
null_ptr <rhannek@gmx.de>
2014-03-13 00:44:46 +0100

    Changed syntax of converter

--- libkeksbot/configs.h
+++ libkeksbot/configs.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <string>
 
 typedef std::map<std::string, std::string> KeyValueMap;
@@ -13,6 +14,67 @@ typedef std::map<std::string, KeyValueMap> DefaultSettings;
 typedef DefaultSettings::value_type DefaultSettingsPair;
 typedef std::map<std::string, SubsectionSettings> SectionSettings;
 typedef SectionSettings::value_type SectionSettingsPair;
+
+template<typename T>
+class Converter
+{
+private:
+	bool success;
+public:
+	T Convert(const std::string& val)
+	{
+		std::istringstream sstr(val);
+		T converted;
+		sstr >> converted;
+		success = !sstr.fail();
+		return converted;
+	}
+	bool WasSuccessful()
+	{
+		return success;
+	}
+};
+template<>
+class Converter<bool>
+{
+private:
+	bool success;
+public:
+	bool Convert(const std::string& val)
+	{
+		if(val == "yes" || val == "1" || val == "y" || val == "true")
+		{
+			success = true;
+			return true;
+		}
+		else if(val == "no" || val == "0" || val == "n" || val == "false")
+		{
+			success = true;
+			return false;
+		}
+		else
+		{
+			success = false;
+			return false;
+		}
+	}
+
+	bool WasSuccessful()
+	{
+		return success;
+	}
+};
+
+template<typename T>
+T ConvertOrDefault(const std::string& val, const T& defaultVal)
+{
+	Converter<T> conv;
+	T converted = conv.Convert(val);
+	if(conv.WasSuccessful())
+		return converted;
+	else
+		return defaultVal;
+}
 
 class Configs
 {
